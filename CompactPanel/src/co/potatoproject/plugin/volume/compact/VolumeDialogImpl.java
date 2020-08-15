e163948213e32f9de6ecb553df16bbd9ab81abc3
poad42 <me@adhityamohan.in>
2020-08-15 06:48:02 +0000

    plugins: Launch Q expanded volume panel on arrow longpress
    * for AospPanel and CompactPanel
    
    Co-authored-by: Mayur <ultramayur123@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- CompactPanel/src/co/potatoproject/plugin/volume/compact/VolumeDialogImpl.java
+++ CompactPanel/src/co/potatoproject/plugin/volume/compact/VolumeDialogImpl.java
@@ -578,10 +578,8 @@ public class VolumeDialogImpl extends PanelSideAware implements VolumeDialog {
 
         if (mExpandRows != null) {
             mExpandRows.setOnLongClickListener(v -> {
-                rescheduleTimeoutH();
                 Events.writeEvent(mContext, Events.EVENT_SETTINGS_CLICK);
-                Intent intent = new Intent(Settings.ACTION_SOUND_SETTINGS);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                Intent intent = new Intent(Settings.Panel.ACTION_VOLUME);
                 dismissH(DISMISS_REASON_SETTINGS_CLICKED);
                 PluginDependency.get(this, ActivityStarter.class).startActivity(intent,
                         true /* dismissShade */);
@@ -611,7 +609,7 @@ public class VolumeDialogImpl extends PanelSideAware implements VolumeDialog {
                     mActivityManager.getLockTaskModeState() == LOCK_TASK_MODE_NONE &&
                             isBluetoothA2dpConnected() ? VISIBLE : GONE);
         }
-        
+
         if (mMediaOutputIcon != null) {
             mMediaOutputIcon.setOnClickListener(v -> {
                 Events.writeEvent(mContext, Events.EVENT_SETTINGS_CLICK);
