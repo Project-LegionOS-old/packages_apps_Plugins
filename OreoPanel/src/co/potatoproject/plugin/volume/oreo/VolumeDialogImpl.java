ddee38e8252ea6847abb899e6e95e5347a603ce3
Pranav Vashi <neobuddy89@gmail.com>
2021-05-13 19:57:17 +0530

    VolumePanel: Add changes for live volume steps
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Id841b03701191b50eefa054e31044f13206ee51d
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- OreoPanel/src/co/potatoproject/plugin/volume/oreo/VolumeDialogImpl.java
+++ OreoPanel/src/co/potatoproject/plugin/volume/oreo/VolumeDialogImpl.java
@@ -929,7 +929,8 @@ public class VolumeDialogImpl implements VolumeDialog {
 
         // update slider max
         final int max = ss.levelMax * 100;
-        if (max != row.slider.getMax()) {
+        final boolean maxChanged = max != row.slider.getMax();
+        if (maxChanged) {
             row.slider.setMax(max);
         }
 
@@ -1009,7 +1010,7 @@ public class VolumeDialogImpl implements VolumeDialog {
         final boolean enableSlider = !zenMuted;
         final int vlevel = row.ss.muted && (!isRingStream && !zenMuted) ? 0
                 : row.ss.level;
-        updateVolumeRowSliderH(row, enableSlider, vlevel);
+        updateVolumeRowSliderH(row, enableSlider, vlevel, maxChanged);
     }
 
     private void updateVolumeRowSliderTintH(VolumeRow row, boolean isActive) {
@@ -1022,7 +1023,7 @@ public class VolumeDialogImpl implements VolumeDialog {
         row.slider.setThumbTintList(tint);
     }
 
-    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel) {
+    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel, boolean maxChanged) {
         row.slider.setEnabled(enable);
         updateVolumeRowSliderTintH(row, row.stream == mActiveStream);
         if (row.tracking) {
@@ -1046,7 +1047,7 @@ public class VolumeDialogImpl implements VolumeDialog {
             }
         }
         final int newProgress = vlevel * 100;
-        if (progress != newProgress) {
+        if (progress != newProgress || maxChanged) {
             if (mShowing && rowVisible) {
                 // animate!
                 if (row.anim != null && row.anim.isRunning()
